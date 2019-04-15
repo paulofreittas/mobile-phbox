f4cb680689009bc35bc08e87839cd12949454dc5
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2019-04-14 23:40:19 -0300

    App react-native Deepbox

---
+++ src/pages/Box/index.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import { Text, View, FlatList, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import ImagePicker from 'react-native-image-picker';
+import RNFS from 'react-native-fs';
+import FileViewer from 'react-native-file-viewer';
+import socket from 'socket.io-client';
+
+import { distanceInWords } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import styles from './styles';
+
+import api from '../../services/api';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default class Box extends Component {
+    state = {
+        box: {}
+    };
+
+    async componentDidMount() {
+        const box = await AsyncStorage.getItem('@deepbox:box');
+        this.subscribeToNewFiles(box);
+        const response = await api.get(`boxes/${box}`);
+
+        this.setState({ box: response.data });
+    }
+
+    subscribeToNewFiles = box => {
+        const io = socket('https://phbox-backend1.herokuapp.com');
+
+        io.emit('connectRoom', box);
+
+        io.on('file', data => {
+            this.setState({
+                box: {
+                    ...this.state.box,
+                    files: [data, ...this.state.box.files]
+                }
+            });
+        });
+    };
+
+    openFile = async file => {
+        try {
+            const filePath = `${RNFS.DocumentDirectoryPath}/${file.title}`;
+
+            await RNFS.downloadFile({
+                fromUrl: file.url,
+                toFile: filePath
+            });
+
+            await FileViewer.open(filePath);
+        } catch (err) {
+            console.log('Arquivo não suportado');
+        }
+    };
+
+    renderItem = ({ item }) => (
+        <TouchableOpacity
+            onPress={() => this.openFile(item)}
+            style={styles.file}>
+            <View style={styles.fileInfo}>
+                <Icon name='insert-drive-file' size={24} color='#d0a672' />
+                <Text style={styles.fileTitle}>{item.title}</Text>
+            </View>
+
+            <Text style={styles.fileDate}>
+                Há{' '}
+                {distanceInWords(item.createdAt, new Date(), {
+                    locale: pt
+                })}
+            </Text>
+        </TouchableOpacity>
+    );
+
+    handleUpload = () => {
+        ImagePicker.launchImageLibrary({}, async upload => {
+            if (upload.error) {
+                console.log('ImagePicker error');
+            } else if (upload.didCancel) {
+                console.log('Canceled by user');
+            } else {
+                const data = new FormData();
+
+                const [prefix, suffix] = upload.fileName.split('.');
+                const ext = suffix.toLowerCase() === 'heic' ? 'jpg' : suffix;
+
+                data.append('file', {
+                    uri: upload.uri,
+                    type: upload.type,
+                    name: `${prefix}.${ext}`
+                });
+
+                api.post(`boxes/${this.state.box._id}/files`, data);
+            }
+        });
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.boxTitle}>{this.state.box.title}</Text>
+                <FlatList
+                    style={styles.list}
+                    data={this.state.box.files}
+                    keyExtractor={file => file._id}
+                    ItemSeparatorComponent={() => (
+                        <View style={styles.separator} />
+                    )}
+                    renderItem={this.renderItem}
+                />
+
+                <TouchableOpacity
+                    style={styles.fab}
+                    onPress={this.handleUpload}>
+                    <Icon name='cloud-upload' size={24} color='#FFF' />
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
